709e2e34e2e62a89490e42bcbaaa058ca4a55fa8
Andrii Badekha <badekha@gmail.com>
2020-11-20 12:04:25 +0200

    Updates

---
+++ precache-manifest.8e579d6c2fb70cccd88fdce40dab5ab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae00c3610297d900b7d042d921dcd0a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "feeb64e34f3396cf347a",
+    "url": "./static/css/main.25379f0d.chunk.css"
+  },
+  {
+    "revision": "7664d83b62f5238e7c6c",
+    "url": "./static/js/2.941ce68b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.941ce68b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feeb64e34f3396cf347a",
+    "url": "./static/js/main.cb178abc.chunk.js"
+  },
+  {
+    "revision": "93b65d15acd366da5855",
+    "url": "./static/js/runtime-main.2badbef3.js"
+  }
+]);
